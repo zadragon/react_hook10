f8ed692b1cc0217b9c8fc7925f10c3c88eebf53b
zadragon <mase0804@naver.com>
2022-02-21 21:52:34 +0900

    2.6 update

--- src/useNetwork.js
+++ src/useNetwork.js
@@ -1,14 +1,29 @@
 import React, {useEffect, useRef, useState} from "react";
 
 const useNetWork = onChange => {
-
+    const [status, setStatus] = useState(navigator.onLine);
+    const handleChange = () => {
+        if(typeof onChange === "function"){
+            onChange(navigator.offline)
+        }
+        setStatus(navigator.offline)
+    }
+    useEffect(()=>{
+        window.addEventListener("online",handleChange);
+        window.addEventListener("offline",handleChange);
+        return ()=>{
+            window.removeEventListener("online",handleChange);
+            window.removeEventListener("offline",handleChange);
+        }
+    },[])
+    return status;
 }
 
 function App() {
-
+    const onLIne = useNetWork();
     return (
         <>
-
+            <h1>{onLIne ? "Online" : "Offline"}</h1>
         </>
     );
 }
